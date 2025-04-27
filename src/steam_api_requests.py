ce8ceb2faecea0b002f1acf0f5d5ce30ce01730b
Ismail Fahmy <ismailmn05@gmail.com>
2025-04-27 14:13:50 +0200

    seperated the GET request for game data from main function to it's own function

--- src/steam_api_requests.py
+++ src/steam_api_requests.py
@@ -1,5 +1,9 @@
 import traceback
 import requests
+import time
+from progress import saveProgress
+# from main import GlobalVars
+
 
 
 
@@ -46,3 +50,67 @@ def requestSteamAppsIDs(logger):
 
 
 
+def handleSteamApiResponse(global_vars):
+
+    logger = global_vars.logger
+    apps_remaining_deque = global_vars.apps_remaining_deque
+
+    i = 0 # This will count how many loops we made
+    while len(apps_remaining_deque) > 0:
+        appid = apps_remaining_deque.popleft()
+
+        # test whether the game exists or not
+        # by making request to get the details of the app
+        try:
+            appdetails_req = requests.get(f"https://store.steampowered.com/api/appdetails?appids={appid}")
+
+
+            match appdetails_req.status_code:
+            # all good
+                case 200: 
+                    appdetails = appdetails_req.json()
+                    appdetails = appdetails[str(appid)]
+
+            # Too Many Requests, pause for a while
+                case 429:
+                    logger.error(f'Too many requests. Put App ID {appid} back to deque. Sleep for 10 sec')
+                    apps_remaining_deque.appendleft(appid)
+                    time.sleep(10)
+                    continue
+
+
+            # Access blocked, wait a while
+                case 403:
+                    logger.error(f'Forbidden to access. Put App ID {appid} back to deque. Sleep for 5 min.')
+                    apps_remaining_deque.appendleft(appid)
+                    time.sleep(5 * 60)
+                    continue
+
+                case _ :
+                    continue
+                
+        # This runs if the status code is 200
+        except Exception:
+            logger.info(f"Error in decoding app details request. App id: {appid}")
+            traceback.print_exc(limit=5) # Print last 5 call stack traces (look it up...)
+            appdetails = {'success':False} # not success -> the game does not exist anymore
+
+
+        # add the app id to excluded app id list
+        if appdetails['success'] == False:
+            global_vars.excluded_appid_list.append(appid)
+            logger.info(f'No successful response. Add App ID: {appid} to excluded apps list')
+            continue
+
+        appdetails_data = appdetails['data'] # Access the data table in the Json Response
+        appdetails_data['appid'] = appid
+        global_vars.apps_dict[appid] = appdetails_data
+        logger.info(f"Successfully get content of App ID: {appid}")
+
+
+        i += 1
+        # for each 2500, save a checkpoint
+        if i >= 2500:
+        # if i >= 25: # 25 for testing
+            saveProgress(global_vars)
+            i = 0
